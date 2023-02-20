76ea000e1597a3cbc460e2617ebd7d3ce276614f
Emre AYDIN <aeaydin1@gmail.com>
2023-02-20 22:37:24 +0000

    Added At for safe container access

--- include/utils.h
+++ include/utils.h
@@ -1,8 +1,21 @@
+#include <utility>
+#include <string>
+
 namespace VUtils
 {
-	template<typename Cont>
-	constexpr auto at(const Cont& _cont, int _ind) -> decltype(_cont[_cont.size()])
+	constexpr auto ThrowIfFail(bool _operation, const char* _message) -> void
 	{
-		static_assert(_ind < 0 && _cont.size());
+		if (_operation)
+		{
+			return;
+		}
+		throw std::runtime_error(_message);
+	}
+	
+	template<typename Cont>
+	constexpr auto At(Cont& _cont, const size_t _ind) -> decltype(_cont[_cont.size()])
+	{
+		ThrowIfFail(_ind < _cont.size(), "Index out of bounds for container.");
+		return _cont[_ind];
 	}
 }// namespace VUtils
